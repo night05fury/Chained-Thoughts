b753b9ce77026551c9d1d825a43b064495ae4b13
night05fury <72437985+night05fury@users.noreply.github.com>
2021-12-23 07:57:07 -0800

    Add files via upload
    Scripts used in this Project : Chained Thoughts

---
+++ SceneLoadeer.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneLoadeer : MonoBehaviour
+{
+    public void PlayButton()
+    {
+        SceneManager.LoadScene("scene1.1");
+    }
+   
+}
